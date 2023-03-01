140c9b5a5fcf0e455ac4a74e7c610ebabf00b33e
amir-m-milani <amirmilani08@gmail.com>
2023-03-01 20:00:11 +0330

    Find the Min Value with ternary operator

--- 01-MinMaxIncome/01-CodeAmir.py
+++ 01-MinMaxIncome/01-CodeAmir.py
@@ -3,7 +3,10 @@ b = int(input("Enter the Number: "))
 c = int(input("Enter the Number: "))
 if a > b and a > c:
     print('Max Value: ', a)
+    print(f"Min Value:{b}" if c > b else f"Min Value: {c}")
 elif c > b and c > a:
     print('Max Value: ', c)
+    print(f"Min Value:{b}" if a > b else f"Min Value: {a}")
 else:
     print('Max Value: ', b)
+    print(f"Min Value:{a}" if c > a else f"Min Value: {c}")
